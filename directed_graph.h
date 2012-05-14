2f78b26335f0991e41e8db83e6d1da74ec129efa
Ilya Razenshteyn <ilyaraz@ilyaraz-debian.msu>
2012-05-14 04:08:46 +0400

    stubs

---
+++ directed_graph.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <boost/range/iterator_range.hpp>
+
+#include <vector>
+
+namespace cpp_graph {
+namespace directed_graph {
+
+struct InputArc {
+    int tail;
+    int head;
+};
+
+struct Arc {
+    int tail;
+    int head;
+    int id;
+};
+
+class DirectedGraph {
+public:
+    typedef std::vector<Arc>::const_iterator ArcConstIterator;
+    typedef boost::iterator_range<ArcConstIterator> ArcConstIteratorRange;
+
+    DirectedGraph(int nodesNumber, const std::vector<InputArc> &arcs);
+
+    int getNodesNumber() const;
+    int getArcsNumber() const;
+
+    ArcConstIteratorRange getOutgoingArcs(int nodeID) const;
+    ArcConstIteratorRange getIncomingArcs(int nodeID) const;
+
+    ArcConstIterator getOutgoingArc(int arcID) const;
+    ArcConstIterator getIncomingArc(int arcID) const;
+private:
+};
+
+}
+}
---
+++ undirected_graph.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "directed_graph.h"
+
+#include <utility>
+
+namespace cpp_graph {
+namespace undirected_graph {
+
+struct InputEdge {
+    int v1;
+    int v2;
+};
+
+struct Edge {
+    int v1;
+    int v2;
+    int id;
+};
+
+int arcIDToEdgeID(int arcID) {
+    return arcID / 2;
+}
+
+std::pair<int, int> edgeIDToArcIDs(int edgeID) {
+    return std::make_pair(2 * edgeID, 2 * edgeID + 1);
+}
+
+class UndirectedGraph: public DirectedGraph {
+pubic:
+    UndirectedGraph(int nodesNumber, const std::vector<InputEdge> &edges);
+
+    int getEdgesNumber() const;
+private:
+};
+
+}
+}
